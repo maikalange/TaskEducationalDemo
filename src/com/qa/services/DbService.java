4bbd054156354b43b45c954802335e8eb8d89c4f
Administrator <Administrator@STUDENT09>
2018-07-06 12:30:55 +0100

    centralised exception handling in the initialiseConnection method

--- src/com/qa/services/DbService.java
+++ src/com/qa/services/DbService.java
@@ -31,80 +31,101 @@ public final class DbService {
     private static void initialiseDbConnection() {
         final String CONNECTION_URL = "jdbc:derby://localhost:1527/TasksDb;user=task_user;password=task_user";
         if (driver == null) {
-            driver = (ClientDriver) Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            try {
+                driver = (ClientDriver) Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+                Logger.getLogger(DbService.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         if (conn == null) {
-            conn = DriverManager.getConnection(CONNECTION_URL);
+            try {
+                conn = DriverManager.getConnection(CONNECTION_URL);
+            } catch (SQLException ex) {
+                Logger.getLogger(DbService.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
     public List<Task> findTasksByCategory(Task.Category category) {
+
         String sql = "SELECT ID,TITLE,DESCRIPTION,DUEDATE,DATECREATED FROM TASK_USER.\"Tasks\"     where \"Category\" = '?'";
         List<Task> tasks = new ArrayList<>();
 
         initialiseDbConnection();
-        PreparedStatement statement = conn.prepareStatement(sql);
-        statement.setString(1, category.name());
-        ResultSet results = statement.executeQuery();
-        Task t;
-        while (results.next()) {
-            String title = results.getString("title");
-            String description = results.getString("description");
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, category.name());
+            ResultSet results = statement.executeQuery();
+            Task t;
+            while (results.next()) {
+                String title = results.getString("title");
+                String description = results.getString("description");
 
-            LocalDateTime dueDate = LocalDateTime.parse(results.getString("dueDate"));
-            LocalDateTime dateCreated = LocalDateTime.parse(results.getString("dateCreated"));
-            int id = results.getInt("Id");
+                LocalDateTime dueDate = LocalDateTime.parse(results.getString("dueDate"));
+                LocalDateTime dateCreated = LocalDateTime.parse(results.getString("dateCreated"));
+                int id = results.getInt("Id");
 
-            t = new Task(description, dateCreated, dueDate, category, title, 0);
-            tasks.add(t);
+                t = new Task(description, dateCreated, dueDate, category, title, 0);
+                tasks.add(t);
+            }
+            conn = null;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DbService.class.getName()).log(Level.SEVERE, null, ex);
         }
-        conn = null;
-
         return tasks;
     }
 
     public boolean updateTask(Task task) {
         boolean isUpdateSuccess = false;
+        initialiseDbConnection();
+        try {
+            String updateSQL = "UPDATE TASK_USER.\"Tasks\"  SET     \"Category\" = '?',  \"Title\" ='?',  \"Description\" ='?',  \"DueDate\" ='?' ,\"DateCreated\" ='?'  WHERE Id=?;";
+            PreparedStatement statement = conn.prepareStatement(updateSQL);
+            statement.setString(1, task.getCategory().name());
+            statement.setString(2, task.getTitle());
+            statement.setString(3, task.getDescription());
 
-        String updateSQL = "UPDATE TASK_USER.\"Tasks\"  SET     \"Category\" = '?',  \"Title\" ='?',  \"Description\" ='?',  \"DueDate\" ='?' ,\"DateCreated\" ='?'  WHERE Id=?;";
-        PreparedStatement statement = conn.prepareStatement(updateSQL);
-        statement.setString(1, task.getCategory().name());
-        statement.setString(2, task.getTitle());
-        statement.setString(3, task.getDescription());
+            statement.setDate(4, Date.valueOf(task.getDueDate().toLocalDate()));
+            statement.setDate(5, Date.valueOf(task.getDateCreated().toLocalDate()));
+            statement.setInt(6, task.getId());
 
-        statement.setDate(4, Date.valueOf(task.getDueDate().toLocalDate()));
-        statement.setDate(5, Date.valueOf(task.getDateCreated().toLocalDate()));
-        statement.setInt(6, task.getId());
+            statement.setString(7, updateSQL);
+            statement.executeUpdate();
 
-        statement.setString(7, updateSQL);
-        statement.executeUpdate();
+            isUpdateSuccess = true;
 
-        isUpdateSuccess = true;
-
+        } catch (SQLException ex) {
+            Logger.getLogger(DbService.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return isUpdateSuccess;
     }
 
     public Task findTaskById(int id) {
-
         Task task = null;
         String findSQL = "SELECT Title,Description,DueDate,DateCreated,Category FROM TASK_USER.\"Tasks\" WHERE ID  = ?;";
 
         initialiseDbConnection();
-        PreparedStatement statement = conn.prepareStatement(findSQL);
-        statement.setInt(1, id);
-        ResultSet results = statement.executeQuery();
+        try {
 
-        while (results.next()) {
-            String title = results.getString("Title");
-            String description = results.getString("Description");
-            String category = results.getString("Category");
+            PreparedStatement statement = conn.prepareStatement(findSQL);
+            statement.setInt(1, id);
+            ResultSet results = statement.executeQuery();
 
-            LocalDateTime dueDate = LocalDateTime.parse(results.getString("DueDate"));
-            LocalDateTime dateCreated = LocalDateTime.parse(results.getString("DateCreated"));
-            task = new Task(description, dateCreated, dueDate, Task.Category.valueOf(category), title, id);
-            conn = null;
+            while (results.next()) {
+                String title = results.getString("Title");
+                String description = results.getString("Description");
+                String category = results.getString("Category");
+
+                LocalDateTime dueDate = LocalDateTime.parse(results.getString("DueDate"));
+                LocalDateTime dateCreated = LocalDateTime.parse(results.getString("DateCreated"));
+                task = new Task(description, dateCreated, dueDate, Task.Category.valueOf(category), title, id);
+                conn = null;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DbService.class.getName()).log(Level.SEVERE, null, ex);
         }
-
         return task;
     }
 
@@ -113,20 +134,25 @@ public final class DbService {
 
         initialiseDbConnection();
         String insertSql = "INSERT INTO TASK_USER.\"Tasks\"(\"Title\",\"Description\",\"Category\",\"DateCreated\",\"DueDate\")Values(?,?,?,?,?)";
-        PreparedStatement statement = conn.prepareStatement(insertSql);
+        try {
 
-        statement.setString(1, task.getTitle());
-        statement.setString(2, task.getDescription());
-        statement.setString(3, task.getCategory().toString());
+            PreparedStatement statement = conn.prepareStatement(insertSql);
 
-        statement.setDate(4, java.sql.Date.valueOf(task.getDateCreated().toLocalDate()));
-        statement.setDate(5, java.sql.Date.valueOf(task.getDueDate().toLocalDate()));
-        statement.execute();
+            statement.setString(1, task.getTitle());
+            statement.setString(2, task.getDescription());
+            statement.setString(3, task.getCategory().toString());
 
-        statement.closeOnCompletion();
-        success = true;
-        conn = null;
+            statement.setDate(4, java.sql.Date.valueOf(task.getDateCreated().toLocalDate()));
+            statement.setDate(5, java.sql.Date.valueOf(task.getDueDate().toLocalDate()));
+            statement.execute();
 
+            statement.closeOnCompletion();
+            success = true;
+            conn = null;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DbService.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return success;
     }
 
@@ -135,11 +161,14 @@ public final class DbService {
         boolean isSuccess = false;
 
         initialiseDbConnection();
-        PreparedStatement statement = conn.prepareStatement(sql);
-        statement.setInt(1, id);
-        statement.execute();
-        isSuccess = true;
-
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setInt(1, id);
+            statement.execute();
+            isSuccess = true;
+        } catch (SQLException ex) {
+            Logger.getLogger(DbService.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return isSuccess;
     }
 }
